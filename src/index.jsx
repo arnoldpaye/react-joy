1cb8f66b09afbe58641dc3b90a22d9565c5cce55
Arnold Paye <arnoldpayet@gmail.com>
2025-08-13 12:02:57 -0400

    root

--- src/index.jsx
+++ src/index.jsx
@@ -20,7 +20,13 @@ function FriendlyGreeting({ name }) {
 
 FriendlyGreeting({name: 'Arnold'});
 
-const root = createRoot(document.querySelector('#root'));
+const target = document.querySelector('#root');
+const root = createRoot(target);
 
 const element = React.createElement(FriendlyGreeting, { name: 'Arnex' });
-root.render(element);
+
+const paragraph = document.createElement('p');
+paragraph.textContent = 'This is a dynamically created paragraph.';
+
+// target.appendChild(paragraph);
+root.render(element)
