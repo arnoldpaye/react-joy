7e021df54e2124a2898427cb64c2233bb430e3e5
Arnold Paye <arnoldpayet@gmail.com>
2025-08-13 11:48:16 -0400

    react element

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createRoot } from 'react-dom/client';
 
-function FriendlyGreeting({ name }) {
+/* function FriendlyGreeting({ name }) {
   return (
     <p style={{ fontSize: '1.25rem', textAlign: 'center', color: 'sienna' }}>
       Greetings, {name}!
     </p>
   );
+} */
+
+function FriendlyGreeting({ name }) {
+  return React.createElement(
+    'p',
+    null,
+    `Greetings: ${name}`
+  );
 }
+
+FriendlyGreeting({name: 'Arnold'});
 
 const root = createRoot(document.querySelector('#root'));
 
-// root.render(<FriendlyGreeting name={'Arnold'} />);
 const element = React.createElement(FriendlyGreeting, { name: 'Arnex' });
 root.render(element);
