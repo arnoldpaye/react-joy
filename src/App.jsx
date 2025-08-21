9af750238d5729270dbc7696f06df89fcbb66deb
Arnold Paye <arnoldpayet@gmail.com>
2025-08-21 10:04:33 -0400

    feat: add TextInput component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import LoginForm from './components/LoginForm';
+import TextInput from './components/TextInput';
 
 
 const data = [
@@ -20,26 +21,8 @@ function App() {
 
   return (
     <>
-      <header>
-        <LoginForm />
-      </header>
-      <main>
-        <h1>Some Website</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-        </p>
-      </main>
-      <footer>
-        <LoginForm />
-      </footer>
+      <TextInput label="Name" />
+      <TextInput id="my-email" label="Email" type="email" />
     </>
   )
 }
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function TextInput({ id, label, type }) {
+  const generatedId = React.useId();
+  const appliedId = id || generatedId;
+
+  /* if (typeof appliedId === 'undefined') {
+    appliedId = React.useId();
+  } */
+
+  return (
+    <div className="text-input">
+      <label htmlFor={appliedId}>{label}</label>
+      <input id={appliedId} type={type} />
+    </div>
+  )
+}
+
+export default TextInput;
