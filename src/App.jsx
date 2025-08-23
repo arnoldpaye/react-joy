d752ce2a496cf22b756da9ae5b03cb9f6f3d78db
Arnold Paye <arnoldpayet@gmail.com>
2025-08-23 08:05:14 -0400

    feat: add Counter component and implement custom CSS reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,12 @@
 import React from "react";
-import LoginForm from "./components/LoginForm";
+import Counter from "./components/Counter";
+import "./styles.css";
 
 const INITIAL_STATE = { name: "Ivy" };
 window.stateValues = [INITIAL_STATE];
 
-function generateName() {
-  const names = [
-    "Ivy",
-    "Liam",
-    "Noah",
-    "Emma",
-    "Olivia",
-    "Ava",
-    "Mason",
-    "Ethan",
-    "Sophia",
-    "Mia",
-    "Lucas",
-    "Amelia",
-    "Harper",
-    "Evelyn",
-    "Logan",
-    "Minh",
-    "An",
-    "Linh",
-    "Hoa",
-    "Quynh",
-  ];
-  return names[Math.floor(Math.random() * names.length)];
-}
-
 function App() {
-  const [user, setUser] = React.useState(INITIAL_STATE);
-
-  return (
-    <>
-      <button
-        onClick={() => {
-          const nextUser = { name: generateName() };
-          setUser(nextUser);
-          window.stateValues.push(nextUser);
-        }}
-      >
-        Edit name
-      </button>
-      <div>{JSON.stringify(user, null, 2)}</div>
-    </>
-  );
+  return <Counter name={"Yuu"} />;
 }
 
 export default App;
