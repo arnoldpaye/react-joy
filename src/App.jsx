9265f3b35ebe67d90efab7da99b492c1121c988a
Arnold Paye <arnoldpayet@gmail.com>
2025-08-25 08:58:16 -0400

    feat: implement MouseCoords component to display mouse position

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
 import React from "react";
+import MouseCoords from "./components/MouseCoords";
 
 function App() {
-  const [searchTerm, setSearchTerm] = React.useState("");
-
-  const inputRef = React.useRef();
-
-  React.useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
-  return (
-    <>
-      <header>
-        <img />
-      </header>
-      <main>
-        <form>
-          <input
-            ref={inputRef}
-            value={searchTerm}
-            onChange={(event) => {
-              setSearchTerm(event.target.value);
-            }}
-          />
-          <button>Search</button>
-        </form>
-      </main>
-    </>
-  );
+  return <MouseCoords />;
 }
 
 export default App;
