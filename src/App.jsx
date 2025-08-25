d3ccb754c7153c3567528fce7c5512f7bdb79d5b
Arnold Paye <arnoldpayet@gmail.com>
2025-08-25 09:03:14 -0400

    feat: implement WindowSize component to display current window dimensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import MouseCoords from "./components/MouseCoords";
+import WindowSize from "./components/WindowSize";
 
 function App() {
-  return <MouseCoords />;
+  return <WindowSize />;
 }
 
 export default App;
