741fa2ca2152fcf237a3027459d876416469b225
Arnold Paye <arnoldpayet@gmail.com>
2025-08-26 07:51:14 -0400

    feat: implement MouseTracker component to display mouse position

---
+++ src/components/MouseTracker.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function MouseTracker() {
+  const [mousePosition, setMousePosition] = React.useState({
+    x: 0,
+    y: 0,
+  });
+
+  React.useEffect(() => {
+    // Effect logic
+    function handleMouseMove(event) {
+      console.log("move");
+      setMousePosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    }
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    // Cleanup code:
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <p>
+      {mousePosition.x} / {mousePosition.y}
+    </p>
+  );
+}
+export default MouseTracker;
