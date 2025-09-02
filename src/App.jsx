cbb54ac7cde234aa2bbb375321536b7d102b5548
Arnold Paye <arnoldpayet@gmail.com>
2025-09-02 08:26:18 -0400

    feat: implement grid component and mouse position tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,63 @@
 import React from "react";
-import useSWR from "swr";
 
-import UserCard from "./components/UserCard";
-import Spinner from "./components/Spinner";
-
-/*
-  API INSTRUCTIONS
-  
-  This endpoint expects a GET request.
-  
-  To simulate an error, attach the following
-  query parameter: `simulatedError=true`
-*/
-
-// const ENDPOINT = "https://jor-test-api.vercel.app/api/get-current-user?simulatedError=true";
-const ENDPOINT = "https://jor-test-api.vercel.app/api/get-current-user";
-
-async function fetcher(endpoint) {
-  const response = await fetch(endpoint);
-  const json = await response.json();
-
-  if (!json.ok) {
-    throw json;
-  }
-
-  return json;
-}
+import Grid from "./components/Grid";
 
 function App() {
-  const { data, error } = useSWR(ENDPOINT, fetcher);
-  console.log(data, error);
+  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
 
-  const isLoading = !data && !error;
+  const [numRows, setNumRows] = React.useState(12);
+  const [numCols, setNumCols] = React.useState(12);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
+  const id = React.useId();
 
-  if (error) {
-    return <p>Something went wrong!</p>;
-  }
+  React.useEffect(() => {
+    function handleMouseMove(event) {
+      setMousePosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    }
 
-  if (data?.user) {
-    return <UserCard name={data.user.name} email={data.user.email} />;
-  }
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor={`${id}-rows`}>Rows:</label>
+          <input
+            id={`${id}-rows`}
+            type="range"
+            value={numRows}
+            onChange={(event) => setNumRows(event.target.value)}
+            min={5}
+            max={40}
+          />
+        </div>
+        <p>
+          {mousePosition.x} / {mousePosition.y}
+        </p>
+        <div>
+          <label htmlFor={`${id}-cols`}>Columns:</label>
+          <input
+            id={`${id}-cols`}
+            type="range"
+            value={numCols}
+            onChange={(event) => setNumCols(event.target.value)}
+            min={5}
+            max={40}
+          />
+        </div>
+      </form>
+
+      <Grid numRows={numRows} numCols={numCols} />
+    </>
+  );
 }
 
 export default App;
