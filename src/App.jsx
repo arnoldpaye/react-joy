16cda45123cdb8be372081935b0b17b258cef01b
Arnold Paye <arnoldpayet@gmail.com>
2025-09-08 08:58:31 -0400

    feat: replace course layout with video archives application

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
 import React from "react";
+import VideoPlayer from "./components/VideoPlayer";
 
-import AccountDropdown from "./components/AccountDropdown";
-import CourseIndexLayout from "./components/CourseIndexLayout";
-
-export const UserContext = React.createContext();
+export const PlaybackRateContext = React.createContext();
 
 function App() {
-  const user = useUser();
+  const [playbackRate, setPlaybackRate] = React.useState("1");
 
   return (
-    <UserContext.Provider value={user}>
-      <AccountDropdown />
-      <CourseIndexLayout />
-    </UserContext.Provider>
+    <PlaybackRateContext.Provider value={{ playbackRate, setPlaybackRate }}>
+      <main>
+        <h1>Video Archives</h1>
+        {DATA.map(({ id, video, createdBy, license }) => (
+          <article key={id}>
+            <VideoPlayer src={video.src} caption={video.caption} />
+            <dl>
+              <dt>Created by</dt>
+              <dd>{createdBy}</dd>
+              <dt>Licensed under</dt>
+              <dd>{license}</dd>
+            </dl>
+          </article>
+        ))}
+      </main>
+    </PlaybackRateContext.Provider>
   );
 }
 
-function useUser() {
-  return {
-    email: "user@email.com",
-  };
-}
+const DATA = [
+  {
+    id: "snowstorm",
+    video: {
+      src: "https://sandpack-bundler.vercel.app/videos/snowstorm.mp4",
+      caption: "A peaceful snowstorm in a residential area",
+    },
+    createdBy: "Karolina Grabowska",
+    license: "Creative Commons Zero (CC0)",
+  },
+  {
+    id: "flowers",
+    video: {
+      src: "https://sandpack-bundler.vercel.app/videos/flowers.mp4",
+      caption: "Macro video of a flower blowing in the wind",
+    },
+    createdBy: "Imam Hossain",
+    license: "Creative Commons Zero (CC0)",
+  },
+  {
+    id: "plane",
+    video: {
+      src: "https://sandpack-bundler.vercel.app/videos/plane.mp4",
+      caption: "Plane flying over the clouds",
+    },
+    createdBy: "Ahmet Akpolat",
+    license: "Creative Commons Zero (CC0)",
+  },
+];
 
 export default App;
