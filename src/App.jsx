d65866494adcd223b0f7bbc7176cfb7456d51e35
Arnold Paye <arnoldpayet@gmail.com>
2025-09-09 10:48:37 -0400

    feat: replace counter/color picker demo with accessible modal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 
-import FavouriteColorProvider from "./FavouriteColorProvider";
-import Counter from "./components/Counter";
-import ColorPicker from "./components/ColorPicker";
+import Modal from "./components/Modal";
+import useToggle from "./hooks/use-toggle";
 
 function App() {
-  const [count, setCount] = React.useState(0);
+  const [isModalOpen, setIsModalOpen] = useToggle(false);
 
   return (
     <>
-      <FavouriteColorProvider>
-        <Counter count={count} setCount={setCount} />
-        <ColorPicker />
-      </FavouriteColorProvider>
-      <p>Current count: {count}</p>
+      {isModalOpen && (
+        <Modal title={"Hello World"} handleDismiss={setIsModalOpen}>
+          This is an example modal! It includes <a href="">several</a>{" "}
+          <a href="">different</a> <a href="">links</a>.
+        </Modal>
+      )}
+
+      <button onClick={setIsModalOpen}>Toggle modal</button>
+
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </p>
+      <p>
+        It was popularised in the 1960s with the release of Letraset sheets
+        containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+      <p>
+        <a href="https://www.lipsum.com/">Read more on lipsum.com</a>.
+      </p>
     </>
   );
 }
